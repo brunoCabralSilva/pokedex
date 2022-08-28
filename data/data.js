bfee051ee2d4fd1d287d56b3379fc201fa9b38c5
brunoCabralSilva <bruno.cabral.silva2018@gmail.com>
2022-08-28 11:24:05 -0300

    Corrigindo projeto para executar no gh-pages

--- data/data.js
+++ data/data.js
@@ -1,91 +1,91 @@
 export const type = [
     {
       tipo: 'Normal',
-      image: '../imagens/type-logo/normal.png',
+      image: './imagens/type-logo/normal.png',
       id: 'normal',
     },
     {
       tipo: 'Fogo',
-      image: '../imagens/type-logo/fire.png',
+      image: './imagens/type-logo/fire.png',
       id: 'fire',
     },
     {
       tipo: 'Água',
-      image: '../imagens/type-logo/water.png',
+      image: './imagens/type-logo/water.png',
       id: 'water',
     },
     {
       tipo: 'Grama',
-      image: '../imagens/type-logo/grass.png',
+      image: './imagens/type-logo/grass.png',
       id: 'grass',
     },
     {
       tipo: 'Voador',
-      image: '../imagens/type-logo/flying.png',
+      image: './imagens/type-logo/flying.png',
       id: 'flying',
     },
     {
       tipo: 'Lutador',
-      image: '../imagens/type-logo/fighting.png',
+      image: './imagens/type-logo/fighting.png',
       id: 'fighting',
     },
     {
       tipo: 'Elétrico',
-      image: '../imagens/type-logo/electric.png',
+      image: './imagens/type-logo/electric.png',
       id: 'electric',
     },
     {
       tipo: 'Venenoso',
-      image: '../imagens/type-logo/poison.png',
+      image: './imagens/type-logo/poison.png',
       id: 'poison',
     },
     {
       tipo: 'Terrestre',
-      image: '../imagens/type-logo/ground.png',
+      image: './imagens/type-logo/ground.png',
       id: 'ground',
     },
     {
       tipo: 'Pedra',
-      image: '../imagens/type-logo/rock.png',
+      image: './imagens/type-logo/rock.png',
       id: 'rock',
     },
     {
       tipo: 'Psíquico',
-      image: '../imagens/type-logo/psychic.png',
+      image: './imagens/type-logo/psychic.png',
       id: 'psychic',
     },
     {
       tipo: 'Gelo',
-      image: '../imagens/type-logo/ice.png',
+      image: './imagens/type-logo/ice.png',
       id: 'ice',
     },
     {
       tipo: 'Inseto',
-      image: '../imagens/type-logo/bug.png',
+      image: './imagens/type-logo/bug.png',
       id: 'bug',
     },
     {
       tipo: 'Fantasma',
-      image: '../imagens/type-logo/ghost.png',
+      image: './imagens/type-logo/ghost.png',
       id: 'ghost'
     },
     {
       tipo: 'Aço',
-      image: '../imagens/type-logo/steel.png',
+      image: './imagens/type-logo/steel.png',
       id: 'steel',
     },
     {
       tipo: 'Dragão',
-      image: '../imagens/type-logo/dragon.png',
+      image: './imagens/type-logo/dragon.png',
       id: 'dragon',
     },
     {
       tipo: 'Sombrio',
-      image: '../imagens/type-logo/dark.png',
+      image: './imagens/type-logo/dark.png',
       id: 'dark',
     },
     {
       tipo: 'Fada',
-      image: '../imagens/type-logo/fairy.png',
+      image: './imagens/type-logo/fairy.png',
       id: 'fairy',
     }];
